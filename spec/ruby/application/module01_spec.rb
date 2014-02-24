cd12098e1969fd3def6a4c07a4aff7e0873f4f8c
Hiroyuki Sano <sh19910711@gmail.com>
2014-02-24 13:22:12 +0900

    Edit module01_spec.rb

--- spec/ruby/application/module01_spec.rb
+++ spec/ruby/application/module01_spec.rb
@@ -9,9 +9,11 @@ describe Application::Module01 do
     it :get_one do
       expect(@module.get_one()).to equal 1
     end
+
     it :get_two do
       expect(@module.get_two()).to equal 2
     end
+
     it :get_three do
       expect(@module.get_three()).to equal 3
     end
