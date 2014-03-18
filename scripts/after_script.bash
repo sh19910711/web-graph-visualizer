3c64153074da823e3df249ffec88f6bd691b60c5
Hiroyuki Sano <sh19910711@gmail.com>
2014-03-18 17:39:52 +0900

    Edit .travis.yml
    各種スクリプトを別ファイルに分割

---
+++ scripts/after_script.bash
@@ -0,0 +1,10 @@
+if [ "${TRAVIS_PULL_REQUEST}" == false ] && [ "${TRAVIS_BRANCH}" == "develop" ]; then
+  git clone --quiet --single-branch -b gh-pages git://github.com/sh19910711/web-graph-visualizer.git tmp/gh-pages \
+    && cp -r doc tmp/gh-pages/ \
+    && cd tmp/gh-pages/ \
+    && git config --global user.email "sh19910711+ynkkowrv0b14@gmail.com" \
+    && git config --global user.name "Mr. Bot" \
+    && git add doc \
+    && git commit -m 'Update Document' \
+    && git push --quiet https://${GITHUB_TOKEN}@github.com/sh19910711/web-graph-visualizer.git gh-pages
+fi
---
+++ scripts/test.bash
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# テストを実行する
+bundle exec rake test > tmp/test/result-ruby.txt
+npm run-script test \
+
+# タグなどの修正
+for RESULT_PATH in `ls tmp/test/result-*.txt`; do
+  cat ${RESULT_PATH} | sed -e "s/\[TODO\]/# TODO/" > `echo ${RESULT_PATH}`.2
+done
+
+# テスト結果の検証
+prove tmp/test/result-*.txt.2 --exec "cat"
+
+# ドキュメント作成
+bundle exec rake doc
+npm run-script doc
+
